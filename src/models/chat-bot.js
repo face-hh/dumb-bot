c926143f9ec4dc65b1f608ff93d2fe44e098bfb9
SuperSweatyTejas <77419101+SuperSweatyTejas@users.noreply.github.com>
2021-08-08 13:34:27 -0400

    Create chat-bot.js

---
+++ src/models/chat-bot.js
@@ -0,0 +1,6 @@
+const { Schema, model } = require('mongoose');
+
+module.exports = model('chat-bot', Schema({
+	guildID: { type: String, required: true },
+    channelID: { type: String, required: true },
+}));
