c5318a38c32e8c50c67eea9913b885b021cc6cc4
SuperSweatyTejas <77419101+SuperSweatyTejas@users.noreply.github.com>
2021-08-08 12:37:07 -0400

    mention prefix + support for aliases

--- src/events/message.js
+++ src/events/message.js
@@ -4,19 +4,25 @@ module.exports = async (pepe, message) => {
 	if (message.author.bot || !message.guild) {
 		return;
 	}
+	let prefix;
 
-	const prefix = util.prefix;
+	let mentionRegex = message.content.match(new RegExp(`^<@!?(${pepe.user.id})>`, 'gi'))
+	if (mentionRegex) {
+	  prefix = `${mentionRegex[0]} `
+	} else {
+	  prefix = util.prefix
+	}
+	
+	if (message.content.indexOf(prefix) !== 0) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
-
-	const requiredCommand = args.shift().toLowerCase();
-
-	const command = pepe.commands.get(requiredCommand);
-
-	if (!command) return;
-
+	const command = args.shift().toLowerCase();
+  
+	const cmd = pepe.commands.get(command) || pepe.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(command));
+	if (!cmd) return
+  
 	try {
-		command.run(pepe, message, args, util);
+		cmd.run(pepe, message, args, util);
 	}
 	catch (error) {
 		message.channel.send('Hmm error appeared :rolling_eyes:\n```md\n# ' + error + '```');
