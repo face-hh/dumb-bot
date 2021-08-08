81db57b9392e2c247c0829b842658f768492e6dc
ChinGH <86087456+chinghytb@users.noreply.github.com>
2021-08-08 20:35:09 +0800

    Add files via upload

---
+++ src/commands/soundboard/benten.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const path = require('path');
+const checkifalreadyplaying = new Discord.Collection();
+module.exports = {
+    name: 'benten',
+    description: 'benten sound board',
+    run: async (client, message, args,) => {
+        const channel = message.member.voice.channel;
+        if (!channel) return message.channel.send('Please connect to a voice channel to use soundboard');
+        channel.join().then(async connection => {
+            const dispatcher = connection.play("https://cdn.discordapp.com/attachments/873617811824599100/873860297943810058/Ben_10_The_Music_Festival_The_Sound_and_The_Furry_Cartoon_Network_mp3cut.net.mp3");
+            const e = await message.react('🎙️');
+            dispatcher.on('speaking', speaking => {
+                if (!speaking) {
+                    channel.leave();
+                    e.remove()
+                }
+            });
+        }).catch(err => console.log(err));
+    }
+}
