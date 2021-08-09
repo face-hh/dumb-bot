afe139ede48aaedb69ec5f16fb9c9cd0ba3ef8f1
SuperSweatyTejas <77419101+SuperSweatyTejas@users.noreply.github.com>
2021-08-09 18:05:47 -0400

    getCase function + changed connection parameters

--- src/utility/mongo.js
+++ src/utility/mongo.js
@@ -1,33 +1,63 @@
-/* eslint-disable valid-typeof */
 const mongoose = require('mongoose');
 const battleDB = require('../models/battle');
-
+const caseDB = require('../models/case')
 mongoose.set('useFindAndModify', false);
 
 module.exports = {
 	/**
 	 * @param {string} uri - The mongoose uri to connect
-	*/
+	 */
 	async connect(uri) {
 		if (!uri) throw new Error('The mongoose uri was not specified in the connect process.');
 		mongoose.connect(uri, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
+			useFindAndModify: false,
+			autoIndex: false
 		});
 	},
 	/**
 	 * @param {string} id - The id used to find the user's data
-	*/
+	 */
 	async getData(id) {
-		const extractedData = await battleDB.findOne({ UserID: id });
-
-		if (!extractedData || typeof extractedData == null) {
-			const newBattle = new battleDB({ UserID: id }).save();
-			return newBattle;
-		}
-		else {
-			console.log(extractedData);
-			return extractedData;
-		}
+		await battleDB.findOne({
+			UserID: id
+		}).lean().exec().then(async (extractedData) => {
+			if (!extractedData) {
+				const newBattle = new battleDB({
+					UserID: id
+				});
+				const {
+					UserID,
+				} = newBattle;
+				await newBattle.save().catch((err) => {
+					console.log(err);
+				});
+				return {
+					UserID,
+				};
+			} else {
+				const {
+					UserID,
+				} = extractedData;
+				return {
+					UserID,
+				};
+			}
+		});
 	},
+
+	async getCase(guildID) {
+		if (!guildID) {
+			throw new Error("Provide guild ID for GetCase function")
+		}
+		const data = await caseDB.findOne({
+			guildID: guildID
+		})
+		if (!data) {
+			return false;
+		} else {
+			return data.case;
+		}
+	}
 };
