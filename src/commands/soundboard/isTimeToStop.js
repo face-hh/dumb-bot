cdc5cfb1c238aae15e25009b8193c905cd0fae37
ChinGH <86087456+chinghytb@users.noreply.github.com>
2021-08-09 18:19:05 +0800

    Update isTimeToStop.js

--- src/commands/soundboard/isTimeToStop.js
+++ src/commands/soundboard/isTimeToStop.js
@@ -4,7 +4,7 @@ const checkifalreadyplaying = new Discord.Collection();
 module.exports = {
     name: 'timestop',
     aliases: [],
-    category: 'fun',
+    category: 'soundboard',
     description: 'timestop sound board',
     disabled: false,
     ownersOnly: false,
