5486bcf5a055b9e894c48089427e5df750ab4449
ImFace <78975742+ImFace@users.noreply.github.com>
2021-08-09 15:13:04 +0300

    everything i did with canvas

--- src/commands/battle/duel.js
+++ src/commands/battle/duel.js
@@ -1,4 +1,7 @@
 const { MessageEmbed, MessageAttachment } = require('discord.js');
+const { MessageButton, MessageActionRow } = require('discord-buttons');
+const { createCanvas, registerFont, loadImage } = require('canvas');
+
 module.exports = {
 	name: 'duel',
 	aliases: [],
@@ -8,97 +11,164 @@ module.exports = {
 	ownersOnly: false,
 	cooldown: 10000,
 	expectedArgs: [['user', 'required']],
-	run: async function(pepe, message, args) {
+	run: async function (pepe, message, args) {
 
 		const mentioned = message.mentions.users.first() || pepe.users.cache.get(args[0]);
 
 		if (!mentioned) return message.channel.send('Mention a user or get friends lmao..');
 
-		const mentionedUser = await pepe.database.getData(mentioned.id);
-		const user = await pepe.database.getData(message.author.id);
+		const row = new MessageActionRow()
+		const row2 = new MessageActionRow()
+		const row3 = new MessageActionRow()
+		const battle_data = require('../../utility/battle_data')
 
-		const { createCanvas, registerFont } = require('canvas');
+		let clickedUsers = []
 
-		registerFont('McFont.otf', { family: 'minecraft' });
+		const gameData = [];
 
-		const canvas = createCanvas(970, 546);
+		battle_data.pepes.forEach((obj) => {
+			if (row.components.length < 5) {
+				row.addComponent(
+					new MessageButton()
+						.setEmoji(obj.emoji)
+						.setLabel(obj.realName)
+						.setID(obj.names[0])
+						.setStyle('green')
+				)
+			} else if (row2.components.length > 5) {
+				row2.addComponent(
+					new MessageButton()
+						.setEmoji(obj.emoji)
+						.setLabel(obj.realName)
+						.setID(obj.names[0])
+						.setStyle('green')
+				)
+			} else if (row3.components.length > 5) {
+				row3.addComponent(
+					new MessageButton()
+						.setEmoji(obj.emoji)
+						.setLabel(obj.realName)
+						.setID(obj.names[0])
+						.setStyle('green')
+				)
+			}
+		})
+		const rows = { components: [] }
+		rows.components.push(row)
+		if (row2.components.length > 5) rows.components.push(row2)
+		if (row3.components.length > 5) rows.components.push(row3)
 
-		const ctx = canvas.getContext('2d');
+		message.channel.send('Choose your pepe!', rows).then((msg) => {
+			let i = 0
+			const collector = msg.createButtonCollector(b => b.clicker.user.id == message.author.id || b.clicker.user.id == mentioned.id)
 
-		const gameData = [
-			{
-				memer: mentionedUser.Hero,
-				healthMax: mentionedUser.HeroHP,
-				healthNow: mentionedUser.HeroHP,
-				shield: mentionedUser.HeroShield,
-			},
-			{
-				memer: user.Hero,
-				healthMax: user.HeroHP,
-				healthNow: user.HeroHP,
-				shield: user.HeroShield,
-			},
-		];
-		// Player One
-		// Max
-		ctx.beginPath();
-		ctx.rect(38, 418, 276, 74);
-		ctx.fillStyle = '#060707';
-		ctx.fill();
-		ctx.stroke();
-		ctx.closePath();
-		// Min
-		ctx.beginPath();
-		ctx.rect(38, 418, gameData[1].healthNow * 276 / gameData[1].healthMax, 74);
-		ctx.fillStyle = '#419656';
-		ctx.fill();
-		ctx.stroke();
-		ctx.closePath();
-		// Text
-		ctx.beginPath();
-		ctx.font = '70px "minecraft"';
-		ctx.fillStyle = '#A4A6A5';
-		ctx.textAllign = 'start';
-		ctx.fillText(`${gameData[1].healthNow}/${gameData[1].healthMax}`, 90, 478);
-		ctx.stroke();
-		ctx.closePath();
+			collector.on('collect', async button => {
+				button.reply.defer()
+				if (clickedUsers.includes(button.clicker.user.id)) return;
+				clickedUsers.push(button.clicker.user.id)
+				i++
 
-		// Player Two
-		// Max
-		ctx.beginPath();
-		ctx.rect(681, 413, 276, 74);
-		ctx.fillStyle = '#060707';
-		ctx.fill();
-		ctx.stroke();
-		ctx.closePath();
-		// Min
-		ctx.beginPath();
-		ctx.rect(681, 413, gameData[0].healthNow * 276 / gameData[0].healthMax, 74);
-		ctx.fillStyle = '#419656';
-		ctx.fill();
-		ctx.stroke();
-		ctx.closePath();
-		// Text
-		ctx.beginPath();
-		ctx.font = '70px "minecraft"';
-		ctx.fillStyle = '#A4A6A5';
-		ctx.textAllign = 'start';
-		ctx.fillText(`${gameData[0].healthNow}/${gameData[0].healthMax}`, 734, 478);
-		ctx.stroke();
-		ctx.closePath();
 
-		const embed = new MessageEmbed()
-			.attachFiles(new MessageAttachment(canvas.toBuffer(), 'pog.png'))
-			.setDescription(`_ _                     ${message.author.username}   **vs**   ${mentioned.username}                     _ _`)
-			.setImage('attachment://pog.png');
-		const { MessageButton, MessageActionRow } = require('discord-buttons');
+				const foundData = battle_data.pepes.find((x) => x.names.includes(button.id))
 
-		const btn = new MessageButton().setID('lol').setEmoji('873937540250484788').setStyle('grey').setLabel('Blade Hit');
-		const btn2 = new MessageButton().setID('lol').setEmoji('873941370568605736').setStyle('red').setLabel('Punch');
-		const btn3 = new MessageButton().setID('lol').setEmoji('873943896110039120').setStyle('blurple').setLabel('Defend');
-		const btn4 = new MessageButton().setID('lol').setEmoji('873944679022997565').setStyle('green').setLabel('Run');
+				gameData.push({
+					memer: foundData.realName,
+					healthMax: foundData.PepeHP,
+					healthNow: foundData.PepeHP,
+					shield: foundData.PepeShield,
+					icon: await loadImage(foundData[`URL${i}`]),
+				})
+				console.log(gameData)
 
-		const row = new MessageActionRow().addComponent(btn).addComponent(btn2).addComponent(btn3).addComponent(btn4);
-		message.channel.send({ embed: embed, component: row });
+				if (gameData.length == 2) startTheGame()
+
+				// Getting and disabling the button 
+				rows.components.forEach(async (messagec) => {
+					let ccac = await messagec.components.find((x) => x.custom_id == button.id)
+					ccac.disabled = true
+					ccac.style = 'red'
+					msg.edit('Choose your pepe!', rows)
+				})
+
+			})
+
+			async function startTheGame() {
+				collector.stop()
+				msg.delete()
+				registerFont('McFont.otf', { family: 'minecraft' });
+
+
+				const canvas = createCanvas(970, 546);
+				console.log(gameData)
+				const ctx = canvas.getContext('2d')
+				ctx.drawImage(gameData[0].icon, 36, 92, 299, 299)
+				ctx.drawImage(gameData[1].icon, 646, 92, 299, 299)
+
+				// Player One
+				// Max
+				ctx.beginPath();
+				ctx.rect(38, 418, 276, 74);
+				ctx.fillStyle = '#060707';
+				ctx.fill();
+				ctx.stroke();
+				ctx.closePath();
+				// Min
+				ctx.beginPath();
+				ctx.rect(38, 418, gameData[1].healthNow * 276 / gameData[1].healthMax, 74);
+				ctx.fillStyle = '#419656';
+				ctx.fill();
+				ctx.stroke();
+				ctx.closePath();
+				// Text
+				ctx.beginPath();
+				ctx.font = '70px "minecraft"';
+				ctx.fillStyle = '#A4A6A5';
+				ctx.textAllign = 'start';
+				ctx.fillText(`${gameData[1].healthNow}/${gameData[1].healthMax}`, 90, 478);
+				ctx.stroke();
+				ctx.closePath();
+
+				// Player Two
+				// Max
+				ctx.beginPath();
+				ctx.rect(681, 413, 276, 74);
+				ctx.fillStyle = '#060707';
+				ctx.fill();
+				ctx.stroke();
+				ctx.closePath();
+				// Min
+				ctx.beginPath();
+				ctx.rect(681, 413, gameData[0].healthNow * 276 / gameData[0].healthMax, 74);
+				ctx.fillStyle = '#419656';
+				ctx.fill();
+				ctx.stroke();
+				ctx.closePath();
+				// Text
+				ctx.beginPath();
+				ctx.font = '70px "minecraft"';
+				ctx.fillStyle = '#A4A6A5';
+				ctx.textAllign = 'start';
+				ctx.fillText(`${gameData[0].healthNow}/${gameData[0].healthMax}`, 734, 478);
+				ctx.stroke();
+				ctx.closePath();
+
+				const embed = new MessageEmbed()
+					.attachFiles(new MessageAttachment(canvas.toBuffer(), 'pog.png'))
+					.setDescription(`${message.author.username}   **vs**   ${mentioned.username}`)
+					.setImage('attachment://pog.png');
+
+				const btn = new MessageButton().setID('lol').setEmoji('873937540250484788').setStyle('grey').setLabel('Blade Hit');
+				const btn2 = new MessageButton().setID('lol').setEmoji('873941370568605736').setStyle('red').setLabel('Punch');
+				const btn3 = new MessageButton().setID('lol').setEmoji('873943896110039120').setStyle('blurple').setLabel('Defend');
+				const btn4 = new MessageButton().setID('lol').setEmoji('873944679022997565').setStyle('green').setLabel('Run');
+
+				const row4 = new MessageActionRow()
+					.addComponent(btn)
+					.addComponent(btn2)
+					.addComponent(btn3)
+					.addComponent(btn4);
+				return message.channel.send({ embed: embed, component: row4 });
+			}
+		})
 	},
 };
