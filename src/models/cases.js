919b47a3fa99a444d62340a0469ab31a5fd957ee
SuperSweatyTejas <77419101+SuperSweatyTejas@users.noreply.github.com>
2021-08-09 18:03:03 -0400

    changed spelling

--- src/models/cases.js
+++ src/models/cases.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 
 module.exports = model('cases', Schema({
-	guildID: { type: String, required: true },
-    case: { type: Number, required: true },
+    guildID: { type: String, required: true },
+    caseID: { type: Number, required: true },
 }));
