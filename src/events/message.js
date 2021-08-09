00ec3caa36a80f7fed5047d217c8f15e1968171a
SuperSweatyTejas <77419101+SuperSweatyTejas@users.noreply.github.com>
2021-08-09 13:21:54 -0400

    cooldowns :kiss:

--- src/events/message.js
+++ src/events/message.js
@@ -7,15 +7,16 @@ module.exports = async (pepe, message) => {
 		return;
 	}
 
-	await chatDB.findOne({ guildID: message.guild.id }, async (err, data) => {
+	await chatDB.findOne({
+		guildID: message.guild.id
+	}, async (err, data) => {
 		if (!data) return;
 		if (message.channel.id !== data.channelID) return;
 		try {
 			const fetched = await fetch(`https://api.affiliateplus.xyz/api/chatbot?message=${encodeURIComponent(message.content)}&botname=${encodeURIComponent('Dumb Bot')}&ownername=${encodeURIComponent('some people lol')}&user=${encodeURIComponent(message.author.id)}`, {});
 			const response = await fetched.json();
 			message.reply(response.message);
-		}
-		catch (e) {
+		} catch (e) {
 			message.reply('Something went wrong while fetching!');
 			console.log(e);
 		}
@@ -35,10 +36,32 @@ module.exports = async (pepe, message) => {
 	const cmd = pepe.commands.get(command) || pepe.commands.find((_cmd) => _cmd.aliases && _cmd.aliases.includes(command));
 	if (!cmd) return;
 
+	const {
+		cooldowns
+	} = pepe;
+
+
+	if (!cooldowns.has(cmd.name)) {
+		cooldowns.set(cmd.name, new Collection());
+	}
+
+	const time = cooldowns.get(cmd.name);
+	let cooldownAmount = cmd.cooldown
+
+	if (time.has(message.author.id)) {
+		const expirationTime = time.get(message.author.id) + cooldownAmount;
+
+		if (Date.now() < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.channel.send(new MessageEmbed().setDescription(`:x: | You are on cooldown for \`${util.prefix}${cmd.name}\`\nPlease wait \`\`${timeLeft.toFixed(1)} seconds\`\` before using **${cmd.name}** command again!`).setColor("RANDOM"))
+		};
+	}
+
+	time.set(message.author.id, now);
+	setTimeout(() => time.delete(message.author.id), cooldownAmount);
 	try {
 		cmd.run(pepe, message, args, util);
-	}
-	catch (error) {
+	} catch (error) {
 		message.channel.send('Hmm error appeared :rolling_eyes:\n```md\n# ' + error + '```');
 		console.log(error);
 	}
